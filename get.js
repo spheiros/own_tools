7e473069d249b5db8c79ba1e7ac47372f26c4837
Xavi Vilallonga <x.vilallonga@gmail.com>
2013-07-17 10:42:00 +0200

    as $_GET for js

---
+++ get.js
@@ -0,0 +1,1 @@
+function gup(name){var regexS="[\?&]"+name+"=([^&#]*)";var regex=new RegExp(regexS);var tmpURL=window.location.href;var results=regex.exec(tmpURL);if(results==null)return "";else return results[1];}
